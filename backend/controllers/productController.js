348d943323b24c0d97d75acb49c08f963f5065ab
Mrkotadiya <nirbhayk7281@gmail.com>
2023-03-22 23:35:02 +0530

    delete products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,40 +14,87 @@ exports.createProduct = async (req, res, next) => {
 // Get All Products
 
 exports.getAllProducts = async (req, res) => {
-  
   const products = await Product.find();
-  
+
   res.status(200).json({
-       success:true,
-       products 
-    });
+    success: true,
+    products,
+  });
 };
-  
 
 // Update Product -- Admin
 
-exports.updateProduct = async(req,res,next)=>{
+// exports.updateProduct = async(req,res,next)=>{
 
-    
+//     let product = await Product.findById(req.params.id);
+//     console.log(product);
+//     if(!product){
+//         return res.status(500).json({
+//             success:false,
+//             message:"Product not Found"
+//         })
+//     }
 
-    let product = await Product.findById(req.params.id);
+//     product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+//         new:true,
+//         runValidators:true,
+//         useFindAndModify:false
+//     });
 
-    if(!product){
-        return res.status(500).json({
-            success:false,
-            message:"Product not Found"
-        })
-    }
+//     res.status(200).json({
+//         success:true,
+//         product
+//     })
 
-    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
-        new:true,
-        runValidators:true,
-        useFindAndModify:false
+// }
+
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not foound",
     });
+  }
 
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// DELETE PRODUCTS
+
+exports.deleteProduct = async (req, res, next) => {
+    console.log("delete ");
+//   const product = await Product.findById(req.params.id);
+
+  await Product.deleteOne({ _id: req.params.id }).then((data) => {
     res.status(200).json({
-        success:true,
-        product
-    })
+      success: true,
+      message: "product Delete Succesfully ",
+      data:data
+    });
+  });
 
-}
+
+//   if(!product){
+//       return res.status(500).json({
+//           success:false,
+//           message:"PRoduct not Found"
+//       })
+//   }
+
+//   await product.remove();
+
+//   res.status(200).json({
+//     success: true,
+//     message: "product Delete Succesfully ",
+//   });
+};
