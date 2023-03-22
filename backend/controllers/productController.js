abf528b488490d34c1e4ca7e78ed81061ed8d044
Mrkotadiya <nirbhayk7281@gmail.com>
2023-03-22 16:00:46 +0530

    product add

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,24 @@
+const Product = require("../models/productModel");
+
+
+
+
+
+// Create PRoduct
+exports.createProduct = async(req,res,next)=>{
+
+    const product= await Product.create(req.body)
+
+    res.status(201).json({
+        success:true,
+        product
+    })
+
+}
+
+
+
+
 exports.getAllProducts = (req,res)=>{
     res.status(200).json({message:"Routes working"})
 }
---
+++ backend/models/productModel.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "Please enter Product Name"],
+  },
+  description: {
+    type: String,
+    require: [true, "Please enter Product description"],
+  },
+  price: {
+    type: Number,
+    require: [true, "Please enter Product Price"],
+    maxLength: [8, "Price cannot Exceed 8 characters"],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        require: true,
+      },
+      url: {
+        type: String,
+        require: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    require: [true, "Please enter Product category"],
+  },
+  stock: {
+    type: Number,
+    require: [true, "Please enter Product Stock"],
+    maxLength: [4, "Stock cannot exceed 4 characters"],
+  },
+  numOfReviews: {
+    type: String,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        require: true,
+      },
+      rating: {
+        type: Number,
+        require: true,
+      },
+      comment: {
+        type: String,
+        require: true,
+      }
+    }
+  ],
+  createdAt:{
+      type:Date,
+      default:Date.now
+  }
+});
+
+
+module.exports = mongoose.model("Product",productSchema);
